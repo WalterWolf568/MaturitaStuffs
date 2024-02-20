4568e4e40fd86ef3adcfe0dc974e9525f80f976f
WalterWolf568 <91047966+WalterWolf568@users.noreply.github.com>
2024-02-20 19:19:01 +0100

    Idk some update or whatever
    Improved the hitboxes (colliders)
    Made the healthbar look better
    Created a script intended for the Level System but didnt code anything yet

---
+++ Assets/Scripts/Game/Player/LevelController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    public int Level = 1;
+    public int Experience = 0;
+}
